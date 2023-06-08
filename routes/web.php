5fa0501e6d3a2f1fad33d292120f802ad4021374
Anggito02 <anggito_anju@yahoo.com>
2023-06-08 19:08:04 +0700

    [CONF] Set view page

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index', [
+    return view('/dashboard/index', [
         "page" => "Home"
     ]);
 });
 
 Route::get('/about', function () {
-    return view('about', [
+    return view('/dashboard/about', [
         "page" => "About"
     ]);
 });
 
 Route::get('/login', function () {
-    return view('login', [
+    return view('/auth/login', [
         "page" => "Login"
     ]);
 });
 
 Route::get('/register', function () {
-    return view('register', [
+    return view('/auth/register', [
         "page" => "Register"
     ]); 
 });
